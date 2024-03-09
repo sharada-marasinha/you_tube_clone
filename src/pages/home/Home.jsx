a8a30c8c9371027e2b2540d5c9b7eb3ac1d49b35
sharada-marasinha <sharadamarasinha@gmail.com>
2024-03-09 15:07:07 +0530

    add : side bar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import './Home.css';
 // import SideBar from "../../Compornent/SideBar/SideBar.jsx";
+import SideBar from "../../Compornent/SideBar/SideBar";
 
 const Home =({sideBar}) =>{
   return(
     <>
-    <div>Hello</div>
-    {/* <SideBar sideBar={sideBar}/> */}
+    {/* <div>Hello</div> */}
+    <SideBar sideBar={sideBar}/>
     </>
   )
 }
